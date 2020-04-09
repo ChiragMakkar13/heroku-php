321f598155dd96fae2c979c5d75256f72968aca9
Chirag Makkar <48089886+ChiragMakkar13@users.noreply.github.com>
2020-04-10 03:07:46 +0530

    Update index.php
    added Image

--- index.php
+++ index.php
@@ -3,8 +3,12 @@
 </body>
 <head>
 <?php 
-echo "hello World";
+echo "hello World "; 
 echo "How are you Today";
 ?>
+<div>
+<img src="http://res.cloudinary.com/khadio-com/image/upload/v1586259561/CI_1_F_SH_A1_4_juhkju.jpg" alt="khadio" /> 
+<p> Below is the Image of E-commerce Product</p>
+</div>
 </head>
 </html>
