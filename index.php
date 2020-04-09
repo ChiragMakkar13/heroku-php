7be56221948ccd5f7da0dfc54260e4d93c3130e0
Chirag Makkar <48089886+ChiragMakkar13@users.noreply.github.com>
2020-04-10 03:19:12 +0530

    Added Heading

--- index.php
+++ index.php
@@ -9,6 +9,9 @@ echo "How are you Today";
 <div>
 <img src="http://res.cloudinary.com/khadio-com/image/upload/v1586259561/CI_1_F_SH_A1_4_juhkju.jpg" alt="khadio" /> 
 <p> Below is the Image of E-commerce Product</p>
+
+<h1> NEw Content </h1>
+
 </div>
 </head>
 </html>
