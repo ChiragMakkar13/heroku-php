5ba9195340e72c4e421d72b446022e0040400a37
ChiragMakkar13 <chiragmakkar94@gmail.com>
2021-06-19 11:30:33 +0530

    Added some changes in the branch

--- index.php
+++ index.php
@@ -5,13 +5,13 @@
 <?php 
 echo "hello World "; 
 echo "How are you Today";
-echo "What we are learning today";
-echo "Hello Echo"
+
+echo "Hello Echo";
 ?>
 <div>
 <p> This is my CICD Piet Branch</p>
 <h1>Testing</h1>
-
+<p> This is my final Branch</p>
 </div>
 </head>
 </html>
